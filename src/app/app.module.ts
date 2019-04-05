0231a00400e42e05a944903b838a9ebea8da8a70
hortasha <eidemartin_303@hotmail.com>
2019-04-05 10:03:44 +0200

    add all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WorkComponent } from './pages/work/work.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectComponent } from './components/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorkComponent,
+    ContactComponent,
+    AboutComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
